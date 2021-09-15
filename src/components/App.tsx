31ed9ebb3d878a9e7c328316d95acf4a449244a4
Florian Marending <florian@marending.dev>
2021-09-15 21:40:36 +0200

    Add dompurify to avoid xss

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { access_share } from '../api/share_api';
 import { decrypt_note } from './crypto';
+import DOMPurify from 'dompurify';
 
 const App = (): React.ReactElement => {
   const path = window.location.pathname.substring(1);
@@ -21,7 +22,14 @@ const App = (): React.ReactElement => {
 
   return (
     <>
-      <div>{content}</div>
+      <div className="h-auto w-full relative">
+        <div className="mx-auto break-words max-w-prose">
+          <div
+            className="prose prose-sm md:prose py-6 px-2"
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
+          />
+        </div>
+      </div>
     </>
   );
 };
