0b5eecd731f14f913dde2005d4a887699ef4cb85
Florian Marending <florian@marending.dev>
2021-09-15 19:59:13 +0200

    Init reader app repo

---
+++ node_modules/@tiptap/react/src/NodeViewContent.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export interface NodeViewContentProps {
+  [key: string]: any,
+  as?: React.ElementType,
+}
+
+export const NodeViewContent: React.FC<NodeViewContentProps> = React.forwardRef((props, ref) => {
+  const Tag = props.as || 'div'
+
+  return (
+    <Tag
+      {...props}
+      ref={ref}
+      data-node-view-content=""
+      style={{
+        ...props.style,
+        whiteSpace: 'pre-wrap',
+      }}
+    />
+  )
+})
