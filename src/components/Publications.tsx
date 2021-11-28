f94d13359b86f5941ea81b35e3f0cbcc6b137b21
Florian Marending <florian@marending.dev>
2021-11-28 14:32:33 +0100

    Add modification date in list

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Publication } from '../api/types';
 import { NOTE_LINK } from './App';
+import { EditIcon } from './icons';
 
 export type Props = {
   publications: Array<Publication>;
@@ -25,14 +26,31 @@ export const Publications = ({
     return `${NOTE_LINK}${publication.token}#${publication.public}`;
   };
 
+  const shouldShowModificationDate = (publication: Publication): boolean => {
+    const created_at = new Date(publication.created_at);
+    const modifiet_at = new Date(publication.modified_at);
+
+    return created_at.toDateString() !== modifiet_at.toDateString();
+  };
+
   return (
-    <div className="mx-auto w-max max-w-full prose prose-sm md:prose px-4 sm:max-w-md md:max-w-lg lg:max-w-2xl">
-      <div className="pt-8 pb-8 font-bold text-2xl">{username}</div>
+    <div className="mx-auto py-8 w-max max-w-full prose prose-sm md:prose px-4 sm:max-w-md md:max-w-lg lg:max-w-2xl">
+      <div className="pb-8 font-bold text-2xl">{username}</div>
       {publications.map((publication: Publication) => (
         <div key={publication.token} className="pb-4">
-          <a href={shareLink(publication)}>{publication.title}</a>
-          <div className="text-gray-500">
-            {creationDate(publication.created_at)}
+          <a className="text-lg" href={shareLink(publication)}>
+            {publication.title}
+          </a>
+          <div className="flex flex-row gap-4">
+            <div className="text-gray-500">
+              {creationDate(publication.created_at)}
+            </div>
+            {shouldShowModificationDate(publication) && (
+              <div className="text-gray-500 flex flex-row gap-1 items-center">
+                <EditIcon className="w-4 h-4" />
+                {creationDate(publication.modified_at)}
+              </div>
+            )}
           </div>
         </div>
       ))}
