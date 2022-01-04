47fba3065667e041741fdc8c35dcd84495e6c91e
Florian Marending <florian@marending.dev>
2022-01-04 20:58:03 +0100

    Make highlight span multiple lines

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -5,10 +5,15 @@ import '../style.css';
 const Headline = (): React.ReactElement => {
   return (
     <div className="flex flex-col pt-16 items-center">
-      <div className="text-4xl font-bold highlight">
-        Minimalist encrypted note taking
+      <div>
+        <span className="text-4xl font-bold highlight">
+          Minimalist encrypted note taking
+        </span>
       </div>
-      <img className="mt-16 shadow-2xl w-3/4" src={screenshot} />
+      <img
+        className="mt-16 shadow-2xl w-3/4 border border-black rounded-md"
+        src={screenshot}
+      />
     </div>
   );
 };
