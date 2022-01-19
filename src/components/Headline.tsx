024a3579baef7092679c7c0a3de66075fa37f31f
Florian Marending <florian@marending.dev>
2022-01-19 23:51:44 +0100

    Make subtitle highlighted

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -5,10 +5,8 @@ import '../style.css';
 const Headline = (): React.ReactElement => {
   return (
     <div className="flex flex-col pt-16 items-center">
-      <div className="text-6xl font-bold highlight">
-        Keep note of the noteworthy
-      </div>
-      <div className="pt-12 text-4xl font-bold">
+      <div className="text-6xl font-bold">Keep note of the noteworthy</div>
+      <div className="pt-12 text-4xl font-bold highlight">
         Minimalist encrypted note taking
       </div>
       <div className="pt-2 text-xl text-center">
